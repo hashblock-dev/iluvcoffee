41c515ba747715a70c730f3f9fbfcf3a93457b40
Cedric <cedric@hashblock.dev>
2025-05-14 23:48:36 +0200

    chore: enabled transform to dto feature

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      whitelist: true, // Strip properties that do not have decorators
+      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
